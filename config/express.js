46caa5c0e23bc66ac0a3b1c363f43f4cfec1db5f
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-16 23:22:16 +0800

    npm install morgan and compress. Modify module.exports

--- config/express.js
+++ config/express.js
@@ -1,1 +1,13 @@
-var express = require('express');
+;var express = require('express');
+var morgan = require('morgan');
+var compress = require('compress');
+
+module.exports = function(){
+  var app = express();
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan('dev'));
+}else if (process.env.NODE_ENV === 'production'){
+  app.use(compress());
+}
+
+};
