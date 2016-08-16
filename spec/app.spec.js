26a6654794daa2f4436564a4c0551708c98cdbfb
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-17 01:55:39 +0800

    update app.spec.js

---
+++ spec/app.spec.js
@@ -0,0 +1,26 @@
+var app = require('../app');
+var request = require("request");
+var supertest = require("supertest");
+var base_url = "http://localhost:3000/";
+
+describe("Express Server API", function() {
+  describe("should return users json on get /kayden", function() {
+      it("returns status code 200", function(done) {
+        supertest(app)
+          .get('/')
+          .expect('Content-Type', /json/)
+          .expect(200, done);
+      });
+
+      xit("returns a correct json format", function(done) {
+        supertest(app)
+          .get('/kayden')
+          .expect({
+            name: 'kai',
+            age: 23,
+            email: 'email@email.com'
+
+          }, done);
+      });
+  });
+});
