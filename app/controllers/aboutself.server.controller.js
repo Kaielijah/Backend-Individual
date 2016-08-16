834f742ca8d350cc74951bdd180993ee2c7a53c3
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-17 01:45:15 +0800

    modify user details in aboutself and created folders

---
+++ app/controllers/aboutself.server.controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+  index: function(req, res) {
+    res.json({
+
+      name: 'kai',
+      age: 23,
+      email: 'email@email.com'
+    });
+  }
+};
--- config/express.js
+++ config/express.js
@@ -25,11 +25,6 @@ module.exports = function(){
   app.use(bodyParser.json());
   app.use(methodOverride());
 
-  app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: config.sessionSecret
-  }));
 
   app.set('view engine', 'ejs');
 
