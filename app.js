4e225f8210b43617f855c601d49592a8c8b23f95
Yi Ting Seah <k.seahyt@gmail.com>
2016-08-17 03:03:13 +0800

    cor

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ var express = require('./config/express'),
 // var db = mongoose();
 var app = express();
 
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+  
 
     app.set('port', (process.env.PORT || 3000));
 
--- config/express.js
+++ config/express.js
@@ -11,11 +11,18 @@ module.exports = function(){
   var app = express();
 
 
+
   if(process.env.NODE_ENV === "development"){
     app.use(morgan('dev'));
   } else if (process.env.NODE_ENV === "production"){
     app.use(compress());
   }
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   app.use(cors());
   app.use(bodyParser.urlencoded({
     extended: true
